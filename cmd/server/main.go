d38c3731de10dfba339591dbaf24a511363dc403
Morteza Aliyari <aliyari.mda@gmail.com>
2021-08-03 21:36:27 +0430

    Structuring our app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Running App")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest Api")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting REST Api")
+		fmt.Println(err)
+	}
+
 }
